ec339cf8d52804b61ae19b51006b1ef64898703f
Andrew Corbin <andrew.c.corbin@gmail.com>
2022-07-07 16:07:19 -0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,51 @@
+import "@rainbow-me/rainbowkit/styles.css";
+
+import {
+  apiProvider,
+  configureChains,
+  getDefaultWallets,
+  RainbowKitProvider,
+  ConnectButton
+} from "@rainbow-me/rainbowkit";
+import { chain, createClient, WagmiProvider } from "wagmi";
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  [apiProvider.alchemy(process.env.ALCHEMY_ID), apiProvider.fallback()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+});
+
+const App = () => {
+  return (
+    <WagmiProvider client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+        >
+          <ConnectButton />
+        </div>
+      </RainbowKitProvider>
+    </WagmiProvider>
+  );
+};
+
+export default App;
